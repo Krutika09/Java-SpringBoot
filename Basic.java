9fedb220778442941611e021c5b9041932c009cf
Krutika Yawale <148604628+Krutika09@users.noreply.github.com>
2025-12-18 09:00:41 +0530

    Add Basic.java with Hello World example

---
+++ Basic.java
@@ -0,0 +1,63 @@
+   class Hello
+   {
+       public static void main(String a[])
+       {
+           System.out.println("Hello World");
+           
+        //   ==========Variables==========
+        //   int num1 = 3;
+        //   int num2 = 4;
+        //   System.out.println(num1 + num2);
+           
+        //   int result = num1 + num2;
+        //   System.out.print(result);
+           
+        // ==========Data type==========
+        //  Primitive data type
+        //  int ==> byte(1byte), short(2byte), int(4byte), long(8byte);
+        //  float ==> double(8byte: ex-5.6), float(4byte : ex-5.6f);
+        //  char ==> letters(2byte) only except single character char 
+        //  ='w';
+        //  bool ==> true/false ex. boolen a=true;
+        // int a = 9;
+        // byte by = 127;
+        // short sh = 777;
+        // long l = 678l;
+            
+        // float f = 5.6f;
+        // double b = 5.9;
+            
+        // char c = 'd';
+            
+        // boolen bool = true;
+        // System.out.print(f);
+        
+        // ========Literals========
+        // Literal is contant value of boolen, numeric, character or string data. Any contant value that can be assigned to variable is called literals.
+        // int num1 = 0b11; Binary literal
+        // int num1 = 0x7E; Hexa-decimal 
+        // double num1 = 1111110011; Octal
+        // System.out.print(num1);
+        
+        // ============Type Conversion and Casting===============
+        // Casting: Data type is converted into another data type by programme(User) using casting operator.
+            // int x ;
+            // byte y = 33;
+            // // byte y = 343;
+            // x=(byte)y;
+            // System.out.print(x);
+            
+            // int x = 156;
+            // int x = 56;
+            // byte y;
+            // y=(byte)x;
+            // System.out.print(y);
+            
+        // Conversion: Data type is automtically converted into another data type by compiler at compiler time.
+            // int x = 290;
+            // float y;
+            // y=x;
+            // System.out.print(y);
+           
+       }
+   }
