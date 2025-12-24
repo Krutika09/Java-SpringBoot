3b6251b9c7bf6c3943f1d14e543ec793a658c37f
Krutika Yawale <148604628+Krutika09@users.noreply.github.com>
2025-12-24 18:19:50 +0530

    Add Demo class with printMessage method

---
+++ Method.java
@@ -0,0 +1,13 @@
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Demo d = new Demo();   // object बनाया
+        d.printMessage();     // method call किया
+    }
+
+    //method 
+    void printMessage() {
+        System.out.println("Hello World");
+    }
+}
