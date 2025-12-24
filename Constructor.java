770889bd5ae12c337aff6d29a7276bf219ddf6f5
Krutika Yawale <148604628+Krutika09@users.noreply.github.com>
2025-12-24 18:18:07 +0530

    Update Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -12,3 +12,21 @@ class CreatingConstructor{
         System.out.println("Constructor is create by: " + obj.name);
     }
 }
+
+
+class Student {
+
+    Student() {   // constructor
+        System.out.println("Constructor called");
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();  // new keyword
+    }
+}
+// 1. Student s1 = new Student();
+// - यहाँ new keyword use हुआ
+// - इससे Student का object बनता है
+// 2. जैसे ही object बना
+// - Constructor अपने आप call हो गया
